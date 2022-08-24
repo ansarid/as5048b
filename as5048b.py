2966163ca5dba14e8d2d9fc2282c6c3ad0b1d5f0
Daniyal Ansari <daniyal.s.ansari+github@gmail.com>
2022-08-24 12:32:24 -0500

    added setup

--- as5048b.py
+++ as5048b.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 '''
-This file is part of the ddcontroller library (https://github.com/ansarid/ddcontroller).
+This file is part of the AS5048B library (https://github.com/ansarid/AS5048B).
 Copyright (C) 2022  Daniyal Ansari
 
 This program is free software: you can redistribute it and/or modify
